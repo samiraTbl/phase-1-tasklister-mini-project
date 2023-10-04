e4bb5f8d7440cbbb4f9f5de57a5cf4344ee01451
samira toubal seghir <samsidt@gmail.com>
2023-10-04 16:19:00 +0200

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form= document.querySelector('#create-task-form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    handleTodo(e.target['new-task-description'].value)
+    form.reset()
+  });
 });
+
+function handleTodo(todo){
+  let li =document.createElement('li')
+  let btn = document.createElement('button')
+btn.addEventListener('click', handleDelete)
+btn.textContent='x'
+  li.textContent= `${todo}  `
+  li.appendChild(btn)
+document.querySelector('#tasks').appendChild(li)
+}
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
